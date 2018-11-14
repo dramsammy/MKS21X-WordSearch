7e9b99d1236198343bcfda75b700fd653161a72f
dramsammy <dramsammy@stuy.edu>
2018-11-14 11:34:20 -0500

    working on constructors

--- WordSearch.java
+++ WordSearch.java
@@ -49,55 +49,55 @@ public class WordSearch{
     returnValue += "Words: " + words + "(Seed:" + Integer.toString(seed) +")";
     return returnValue;
   }
-    public boolean addWordHorizontal(String word,int row, int col){
-      int length = word.length();
-      if (row >= data[0].length || col + length >= data.length){
-        return false;
-      }
-      for (int i = 0; i < length; i++){
-        if(((data[row][col+i]!=('_'))) && (data[row][col+i]!=(word.charAt(i)))) {
-          return false;
-      }
-    }
-      for(int a = 0; a < length; a++){
-        data[row][col+a]=word.charAt(a);
-      }
-      return true;
-    }
-    public boolean addWordVertical(String word,int row, int col){
-      int length = word.length();
-      if (row + length >= data[0].length || length >= data.length){
-        return false;
-      }
-      for (int i = 0; i < length; i++){
-        if(((data[row+i][col]!=('_'))) && (data[row+i][col]!=(word.charAt(i)))) {
-          return false;
-      }
-    }
-      for(int a = 0; a < length; a++){
-        data[row + a][col]=word.charAt(a);
-      }
-      return true;
-    }
-    public boolean addWordDiagonal(String word,int row, int col){
-      int length = word.length();
-      for (int i=0;i<length;i++)
-        if (row + length >= data[i].length || col + length >= data.length){
-          return false;
-      }
-      for (int i=0;i<length;i++) {
-        if (((data[row+i][col + i]!=('_'))) && (data[row+i][col + i]!=(word.charAt(i)))) {
-          return false;
-          }
-        }
-      for (int i=0;i< length;i++) {
-        data[row+i][col+i]=word.charAt(i);
-        }
-          return true;
-      }
+    // public boolean addWordHorizontal(String word,int row, int col){
+    //   int length = word.length();
+    //   if (row >= data[0].length || col + length >= data.length){
+    //     return false;
+    //   }
+    //   for (int i = 0; i < length; i++){
+    //     if(((data[row][col+i]!=('_'))) && (data[row][col+i]!=(word.charAt(i)))) {
+    //       return false;
+    //   }
+    // }
+    //   for(int a = 0; a < length; a++){
+    //     data[row][col+a]=word.charAt(a);
+    //   }
+    //   return true;
+    // }
+    // public boolean addWordVertical(String word,int row, int col){
+    //   int length = word.length();
+    //   if (row + length >= data[0].length || length >= data.length){
+    //     return false;
+    //   }
+    //   for (int i = 0; i < length; i++){
+    //     if(((data[row+i][col]!=('_'))) && (data[row+i][col]!=(word.charAt(i)))) {
+    //       return false;
+    //   }
+    // }
+    //   for(int a = 0; a < length; a++){
+    //     data[row + a][col]=word.charAt(a);
+    //   }
+    //   return true;
+    // }
+    // public boolean addWordDiagonal(String word,int row, int col){
+    //   int length = word.length();
+    //   for (int i=0;i<length;i++)
+    //     if (row + length >= data[i].length || col + length >= data.length){
+    //       return false;
+    //   }
+    //   for (int i=0;i<length;i++) {
+    //     if (((data[row+i][col + i]!=('_'))) && (data[row+i][col + i]!=(word.charAt(i)))) {
+    //       return false;
+    //       }
+    //     }
+    //   for (int i=0;i< length;i++) {
+    //     data[row+i][col+i]=word.charAt(i);
+    //     }
+    //       return true;
+    //}
     public boolean addWord(String word,int row, int col, int rowIncrement, int colIncrement){
       int length = word.length();
-      if (row + (length-1) * rowIncrement  >= data.length || col + (length-1) * colIncrement  >= data.length[0]){
+      if (row + (length-1) * rowIncrement  >= data.length || col + (length-1) * colIncrement  >= data[0].length){
         return false;
           }
       if (rowIncrement ==0 && colIncrement == 0){
@@ -112,8 +112,9 @@ public class WordSearch{
             return false;
             }
           }
-      for (int i=0;i<length);i++) {
+      for (int i=0; i<length; i++) {
         data[row+(i*rowIncrement)][col+(i*colIncrement)]=word.charAt(i);
       }
       return true;
         }
+      }
